77c07bc95ff785e98028db1b3606624d68d9c05b
salman608 <salman608bd@gmail.com>
2021-05-20 20:18:21 +0600

    change vehicle controller

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@
 		var table1 = $(this).parent().parent().parent();
 		var table2 = $(this).parent().parent();
 		var verTable = $(table1).data('vertable')+"";
-		var column = $(this).data('column') + ""; 
+		var column = $(this).data('column') + "";
 
 		$(table2).find("."+column).addClass('hov-column-'+ verTable);
 		$(table1).find(".row100.head ."+column).addClass('hov-column-head-'+ verTable);
@@ -15,11 +15,11 @@
 		var table1 = $(this).parent().parent().parent();
 		var table2 = $(this).parent().parent();
 		var verTable = $(table1).data('vertable')+"";
-		var column = $(this).data('column') + ""; 
+		var column = $(this).data('column') + "";
 
 		$(table2).find("."+column).removeClass('hov-column-'+ verTable);
 		$(table1).find(".row100.head ."+column).removeClass('hov-column-head-'+ verTable);
 	});
-    
+
 
 })(jQuery);
