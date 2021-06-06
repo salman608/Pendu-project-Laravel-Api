7f7acc6e1530200e10a6efd49191ed6c827598e9
salman608 <salman608bd@gmail.com>
2021-06-06 22:00:44 +0600

    problem resolve

--- resources/views/frontend/modal/otp_modal.blade.php
+++ resources/views/frontend/modal/otp_modal.blade.php
@@ -109,77 +109,8 @@ p{
     </div>
 </div>
 
-{{-- <div class="modal fade" id="OtpModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-            <div class="d-flex justify-content-center align-items-center container">
-                <div class="card py-5 px-3">
-                    <img src="{{asset('frontend/images/otp.png')}}" alt="">
-                   <p>One Time Password(OTP) Has been sent to your phone number</p>
-                    <div class="d-flex flex-row mt-5"><input type="text" class="form-control" autofocus=""><input type="text" class="form-control"><input type="text" class="form-control"><input type="text" class="form-control"></div>
-                    <div class="text-center mt-5"><span class="d-block mobile-text">Don't receive the code?</span><span class="font-weight-bold text-danger cursor">Resend</span></div>
-                </div>
-            </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div> --}}
+
 
   <script>
-      $(function() {
-  'use strict';
 
-  var body = $('body');
-
-  function goToNextInput(e) {
-    var key = e.which,
-      t = $(e.target),
-      sib = t.next('input');
-
-    if (key != 9 && (key < 48 || key > 57)) {
-      e.preventDefault();
-      return false;
-    }
-
-    if (key === 9) {
-      return true;
-    }
-
-    if (!sib || !sib.length) {
-      sib = body.find('input').eq(0);
-    }
-    sib.select().focus();
-  }
-
-  function onKeyDown(e) {
-    var key = e.which;
-
-    if (key === 9 || (key >= 48 && key <= 57)) {
-      return true;
-    }
-
-    e.preventDefault();
-    return false;
-  }
-
-  function onFocus(e) {
-    $(e.target).select();
-  }
-
-  body.on('keyup', 'input', goToNextInput);
-  body.on('keydown', 'input', onKeyDown);
-  body.on('click', 'input', onFocus);
-
-});
   </script>
