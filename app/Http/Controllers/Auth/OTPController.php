8c52bc02c746d793298168b5d4abbc748d598846
Masud Rana <MasudRana917775@gmail.com>
2021-07-02 15:42:27 +0600

    refactor

--- app/Http/Controllers/Auth/OTPController.php
+++ app/Http/Controllers/Auth/OTPController.php
@@ -22,10 +22,10 @@ class OTPController extends Controller
      *
      */
     public function verifyPhone(Request $request){
-        $inputCode = $request->num1.$request->num2.$request->num3.$request->num4;
+        $inputCode = $request->num1.$request->num2.$request->num3.$request->num4.$request->num5.$request->num6;
         $code = (int)$inputCode;
 
-        if($code != 1234){
+        if($code != 123456){
             session('error', 'You verification code is invalid.');
 
             return redirect()->back();
